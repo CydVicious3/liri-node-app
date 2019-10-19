2f2677348676714953a42b9dc1b76ff43831a86c
E L A <ela@elamac24.lan>
2019-10-18 20:41:27 -0700

    initial code functions

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# liri-node-app
+1. CLI app that searches Spotify for songs, Bands in Town for concerts, and OMDB for movies.
+2. Give a high-level overview of how the app is organized
+3. Give start-to-finish instructions on how to run the app
+4. Include screenshots, gifs or videos of the app functioning
+5. Contain a link to a deployed version of the app
+6. Clearly list the technologies used in the app
+7. State your role in the app development
---
+++ app.js
@@ -0,0 +1,89 @@
+require("dotenv").config();
+//require file system
+const fs = require('fs');
+// link key page
+var keys = require("./keys.js");
+// initialize spotify
+const Spotify = require('node-spotify-api');
+const spotify = new Spotify(keys.spotify);
+// require axios
+const axios = require('axios');
+//require moment
+const moment = require('moment');
+// omdb and bandsintown api
+let omdb = (keys.omdb);
+let bandsintown = (keys.bandsintown);
+
+// take user command and input
+let userInput = process.argv[2];
+let userQuery = process.argv.slice(3).join(" ");
+console.log(operation)
+console.log(topic)
+
+
+switch (operation) {
+  case 'concert-this':
+    concert()
+    break;
+
+  case 'spotify-this-song':
+    song()
+    break;
+
+  case 'movie-this':
+    movies()
+    break;
+
+  case 'do-what-it-says':
+    whatever()
+    break;
+
+  default:
+    break;
+}
+
+
+//   * `movie-this`
+
+//   * `do-what-it-says`
+
+// * `concert-this`
+function concert() {
+  var queryURL = "https://rest.bandsintown.com/artists/" + topic + "/events?app_id=codingbootcamp"
+  console.log(queryURL)
+
+  axios.get(queryURL).then(function (response) {
+    console.log(response.data)
+  })
+
+  // for every concert (name, venue , country, city, date format with moment)
+  for (i = 0; i < queryURL.length; i++) {
+    text += queryURL[i] + " ";
+  }
+}
+
+// * Artist(s)
+
+//     * The song's name
+
+//       * A preview link of the song from Spotify
+
+//         * The album that the song is from
+
+//           * If no song is provided then your program will default to "The Sign" by Ace of Base.
+function song() {
+  var spotify = new Spotify(keys.spotify);
+  fetch('https://www.npmjs.com/package/node-spotify-api')
+    .then(r => r.json())
+    .then(data => {
+    }
+
+function movies() {
+
+
+      }
+
+function whatever() {
+
+
+      }
