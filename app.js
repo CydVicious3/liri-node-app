51a5cf100456d0d599f789537a8172db414a6e37
E L A <ela@elamac24.lan>
2019-10-20 17:48:55 -0700

    addded spotify song earch

--- app.js
+++ app.js
@@ -13,12 +13,12 @@ const moment = require('moment');
 // omdb and bandsintown api
 let omdb = (keys.omdb);
 let bandsintown = (keys.bandsintown);
+// require geocoder for location
+const NodeGeocoder = require('node-geocoder');
 
 // take user command and input
 let userInput = process.argv[2];
 let userQuery = process.argv.slice(3).join(" ");
-console.log(operation)
-console.log(topic)
 
 // app logic
 switch (userInput) {
@@ -30,13 +30,13 @@ switch (userInput) {
     song()
     break;
 
-  case 'movie-this':
-    movies()
-    break;
+  // case 'movie-this':
+  //   movies()
+  //   break;
 
-  case 'do-what-it-says':
-    whatever()
-    break;
+  // case 'do-what-it-says':
+  //   whatever()
+  //   break;
 
   default:
     console.log("I don't know!")
@@ -51,19 +51,49 @@ switch (userInput) {
 // * `concert-this`
 function concert() {
   console.log(`Searching For ... ${userQuery} Shows!`)
-  let queryURL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"
-  console.log(queryURL)
-
-  axios.get(queryURL).then(function (response) {
+  let queryUrl = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
+  console.log(queryUrl)
+  let userBand = JSON.parse(body);
+  axios.get(queryUrl).then(function (response) {
     console.log(response.data)
   })
-
-  // for every concert (name, venue , country, city, date format with moment)
-  for (i = 0; i < queryURL.length; i++) {
-    text += queryURL[i] + " ";
+  if (userBand.length > 0) {
+    // for every concert (name, venue , country, city, date format with moment)
+    for (i = 0; i < 1; i++) {
+      console.log(`
+      \nArtist: ${userBand[i].lineup[0]} \nVenue: ${userBand[i].venue.name} \nVenue Location: ${userBand[i].venue.city}, ${userBand[i].venue.country}`);
+      let concertDate = moment(userBand[1].datetime).format('MM/DD/YY  hh:00 A');
+      console.log(`Date and Time: ${concertDate}`);
+    }
+  } else {
+    console.log('Band / Concert Not Found!');
   }
+
 }
 
+// function song() {
+//   let spotify = new Spotify(keys.spotify);
+//   fetch('https://www.npmjs.com/package/node-spotify-api')
+//     .then(r => r.json())
+//     .then(data => { }
+// }
+
+function (song) {
+  console.log(`Searching For ... ${userQuery} Song!`);
+  // * If no song is provided then your program will default to "The Sign" by Ace of Base.
+  if (!userQuery) { userQuery = 'the sign ace of base' };
+  spotify.search({ type: 'track', query: userQuery, limit: 1 }, function (error, data) {
+    if (error) {
+      console.log('Error:' + error);
+    }
+    // put spotify data in array
+    let spotifyArr = data.track.items;
+    for (i = 0; i < spotifyArr.length; i = ++) {
+      console.log(`Found this for you: \n\nArtist: ${data.track.items[i].album.artist[0].name} \nSong: ${data.track.items[i].name} \nSong Link: ${data.track.items[i].external_urls.spotify} \nAlbum: ${data.track.items[i].album.name}`)
+    }
+  })
+
+}
 // * Artist(s)
 
 //     * The song's name
@@ -72,20 +102,12 @@ function concert() {
 
 //         * The album that the song is from
 
-//           * If no song is provided then your program will default to "The Sign" by Ace of Base.
-function song() {
-  var spotify = new Spotify(keys.spotify);
-  fetch('https://www.npmjs.com/package/node-spotify-api')
-    .then(r => r.json())
-    .then(data => {
-    }
-
-function movies() {
+// function movies() {
 
 
-      }
+//       }
 
-function whatever() {
+// function whatever() {
 
 
-      }
+//       }
